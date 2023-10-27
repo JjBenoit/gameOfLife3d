6beddc31adf1be9cfb07b136aeb32ca50e3d7b59
jean-jacques benoit <jean-jacques.benoit@dgfip.finances.gouv.fr>
2023-10-27 17:17:15 +0200

    ajout cache

--- src/main/java/launch/RunGameDeterminedDataSetGrid.java
+++ src/main/java/launch/RunGameDeterminedDataSetGrid.java
@@ -4,22 +4,27 @@ import java.io.File;
 
 import game.GameOflife;
 import game.object.Grid;
+import graphixx.GameInfos;
 import graphixx.GridFramePaint;
 import util.GridUtil;
 
-public class RunGameDeterminedDataSetGrid {
+public class RunGameDeterminedDataSetGrid
+{
 
     // Lancement avec un JDD
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
 
-	Grid grid = GridUtil
-		.initGridFromFile(new File("/home/socle_gaia2/soda1/workspace/gameOfLife3d/patterns/planer.xml"));
+        GameInfos gameInfos = new GameInfos();
 
-	GameOflife jeuVie = new GameOflife(grid);
+        Grid grid = GridUtil
+            .initGridFromFile(new File("/home/socle_gaia2/soda1/workspace/gameOfLife3d/patterns/planer.xml"));
 
-	GridFramePaint frame = new GridFramePaint(jeuVie);
+        gameInfos.setGrid(grid);
 
-	frame.go();
+        GridFramePaint frame = new GridFramePaint(gameInfos, new GameOflife(gameInfos));
+
+        frame.startRender();
 
     }
 }
